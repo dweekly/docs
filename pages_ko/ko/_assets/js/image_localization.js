711fc58324f61bd3997db77040ed9aa53ba77f15
Alexandra Oprea <aoprea@avente.com>
2019-07-25 22:47:06 +0300

    Improved image localization

--- pages_ko/ko/_assets/js/image_localization.js
+++ pages_ko/ko/_assets/js/image_localization.js
@@ -1,8 +1,31 @@
 var lang = 'ko';
 
+var shouldReplace = false;
+
+function onDataRetrieved () {
+    if (this.status === 404) {
+        shouldReplace = false;
+    } else {
+        shouldReplace = true;
+    }
+
+}
+
 window.addEventListener('DOMContentLoaded', (event) => {
     var els = document.querySelectorAll('img');
     for (var pos = 0; pos < els.length; pos++) {
-        els[pos].src = els[pos].src.replace('_assets', lang + '/_assets');
+        var newURL = els[pos].src.replace('_assets', lang + '/_assets');
+
+        var xhttp = new XMLHttpRequest();
+        xhttp.addEventListener("load", onDataRetrieved);
+        xhttp.open("GET", newURL, true);
+        xhttp.send();
+        xhttp.close();
+
+        if(shouldReplace) {
+            els[pos].src = newURL;
+        }
+
+        shouldReplace = false;
     }
-)}
+});
