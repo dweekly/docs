b4b1e305a84b22b7307b23fb5a05e041b037b823
Alexandra Oprea <aoprea@avente.com>
2019-07-26 14:24:37 +0300

    Async image localization

--- pages_zh/zh/_assets/js/image_localization.js
+++ pages_zh/zh/_assets/js/image_localization.js
@@ -1,31 +1,20 @@
 var lang = 'zh';
 
-var shouldReplace = false;
+const myAsyncFunc = async (el, newURL) => {
 
-function onDataRetrieved () {
-    if (this.status === 200) {
-        shouldReplace = true;
-    } else {
-        shouldReplace = false;
-    }
-    console.log(this.status);
-
-}
+    await fetch(newURL)
+        .then(function (response) {
+            if (response.status === 200) {
+                el.src = newURL;
+            }
+        });
+};
 
 window.addEventListener('DOMContentLoaded', (event) => {
     var els = document.querySelectorAll('img');
     for (var pos = 0; pos < els.length; pos++) {
         var newURL = els[pos].src.replace('_assets', lang + '/_assets');
 
-        var xhttp = new XMLHttpRequest();
-        xhttp.addEventListener("load", onDataRetrieved);
-        xhttp.open("GET", newURL, false);
-        xhttp.send();
-
-        if(shouldReplace) {
-            els[pos].src = newURL;
-        }
-
-        shouldReplace = false;
+        myAsyncFunc(els[pos], newURL);
     }
 });
